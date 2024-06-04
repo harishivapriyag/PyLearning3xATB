3d38719054dac4c28bc1f72c4d62ad0a507cc56c
harishivapriyag <harishivapriyag.99@gmail.com>
2024-06-04 22:17:28 +0530

    Adding 'Lab001' to commit

---
+++ Ex_03062024/Lab001.py
@@ -0,0 +1,11 @@
+# This is single line comment
+
+print('Hello! Harini')
+print('Hello! Harini')
+print('Hello! Harini')
+print('Hello! Harini')
+print('Hello! World')
+print(3 * 1)
+print(10 / 2)
+print(12 - 5)
+print(1 + 9)
